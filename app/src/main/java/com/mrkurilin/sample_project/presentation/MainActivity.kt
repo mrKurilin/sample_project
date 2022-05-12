25d1b0ae73b7053483d1f5e44b2805468aa13071
mrKurilin <kurilin-anton@mail.ru>
2022-05-12 22:43:21 +0300

    Added splashscreen

--- app/src/main/java/com/mrkurilin/sample_project/presentation/MainActivity.kt
+++ app/src/main/java/com/mrkurilin/sample_project/presentation/MainActivity.kt
@@ -1,14 +1,19 @@
 package com.mrkurilin.sample_project.presentation
 
+import android.animation.ObjectAnimator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.animation.AnticipateInterpolator
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.mrkurilin.sample_project.R
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContentView(R.layout.activity_main)
 
-        // TODO: Add splashscreen
     }
 }
