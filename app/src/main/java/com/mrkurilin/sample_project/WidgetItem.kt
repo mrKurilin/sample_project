7e606f416645b34cf8014c236b672ad727b165ec
mrKurilin <kurilin-anton@mail.ru>
2022-05-19 12:42:22 +0300

    The power of EARTH little cleanup

--- app/src/main/java/com/mrkurilin/sample_project/WidgetItem.kt
+++ app/src/main/java/com/mrkurilin/sample_project/WidgetItem.kt
@@ -5,7 +5,8 @@ data class WidgetItem(val layout: Int) {
     companion object{
         val widgets = arrayListOf(
             WidgetItem(R.layout.widget_checkbox),
-            WidgetItem(R.layout.widget_text_view)
+            WidgetItem(R.layout.widget_text_view),
+            WidgetItem(R.layout.widget_radio_button)
         )
     }
 }
