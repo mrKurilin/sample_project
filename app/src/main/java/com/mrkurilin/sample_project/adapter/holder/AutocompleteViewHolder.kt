3e4ab147015544dcf551f08c6b1fad2f70845b6a
mrKurilin <kurilin-anton@mail.ru>
2022-05-24 12:11:16 +0300

    Remarks

--- app/src/main/java/com/mrkurilin/sample_project/adapter/holder/AutocompleteViewHolder.kt
+++ app/src/main/java/com/mrkurilin/sample_project/adapter/holder/AutocompleteViewHolder.kt
@@ -8,19 +8,20 @@ import androidx.core.view.isVisible
 import com.mrkurilin.sample_project.R
 
 class AutocompleteViewHolder(view: View) : WidgetViewHolder(view) {
+
     private val actionButton = view.findViewById<Button>(R.id.button_autocomplete_textview_widget)
     private val expandablePart = view.findViewById<AutoCompleteTextView>(
         R.id.autocomplete_textview_widget
     )
     private var isExpanded = false
 
-    private val array: Array<String> = view.resources.getStringArray(R.array.sample_array)
+    private val autoCompleteStringArray = view.resources.getStringArray(R.array.widgets_auto_complete_sample)
 
     init {
         val adapter = ArrayAdapter(
             view.context,
             androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,
-            array
+            autoCompleteStringArray
         )
         expandablePart.setAdapter(adapter)
         expandablePart.threshold = 1
