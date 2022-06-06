be3a7bc8e65748ab3e5ee058470cdb0ea7738c45
mrKurilin <kurilin-anton@mail.ru>
2022-06-06 13:04:34 +0300

    Added spinner shit

--- app/src/main/java/com/mrkurilin/sample_project/adapter/holder/SeekBarViewHolder.kt
+++ app/src/main/java/com/mrkurilin/sample_project/adapter/holder/SeekBarViewHolder.kt
@@ -3,38 +3,38 @@ package com.mrkurilin.sample_project.adapter.holder
 import android.graphics.Color
 import android.view.View
 import android.widget.*
-import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.core.view.children
 import androidx.recyclerview.widget.RecyclerView
 import com.mrkurilin.sample_project.R
 
 class SeekBarViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
-    private val viewToChangeColor: View = view.findViewById(R.id.view_seekbar_widget)
+    private val redSeekBar: SeekBar = view.findViewById(R.id.seekbar_red_seekbar_widget)
+    private val greenSeekBar: SeekBar = view.findViewById(R.id.seekbar_green_seekbar_widget)
+    private val blueSeekBar: SeekBar = view.findViewById(R.id.seekbar_blue_seekbar_widget)
+    private val colorSampleView: View = view.findViewById(R.id.view_seekbar_widget)
     private val onSeekBarChangeListener by lazy { createOnSeekBarChangeListener() }
     private var redColor = 0
     private var greenColor = 0
     private var blueColor = 0
 
     init {
-        view.findViewById<ConstraintLayout>(R.id.constraint_layout_seekbar_widget)
-            .children
-            .forEach { childView ->
-            if (childView is SeekBar) childView.setOnSeekBarChangeListener(onSeekBarChangeListener)
-        }
+        redSeekBar.setOnSeekBarChangeListener(onSeekBarChangeListener)
+        greenSeekBar.setOnSeekBarChangeListener(onSeekBarChangeListener)
+        blueSeekBar.setOnSeekBarChangeListener(onSeekBarChangeListener)
     }
 
     private fun createOnSeekBarChangeListener() : SeekBar.OnSeekBarChangeListener{
         return object : SeekBar.OnSeekBarChangeListener{
+
             override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
-                when (seekBar.id) {
-                    R.id.seekbar_red_seekbar_widget -> {
+                when (seekBar) {
+                    redSeekBar-> {
                         redColor = progress
                     }
-                    R.id.seekbar_green_seekbar_widget -> {
+                    greenSeekBar -> {
                         greenColor = progress
                     }
-                    R.id.seekbar_blue_seekbar_widget -> {
+                    blueSeekBar -> {
                         blueColor = progress
                     }
                 }
@@ -52,6 +52,7 @@ class SeekBarViewHolder(view: View) : RecyclerView.ViewHolder(view) {
     }
 
     private fun updateViewColor() {
-        viewToChangeColor.setBackgroundColor(Color.rgb(redColor, greenColor, blueColor))
+        val color = Color.rgb(redColor, greenColor, blueColor)
+        colorSampleView.setBackgroundColor(color)
     }
 }
--- app/src/main/java/com/mrkurilin/sample_project/adapter/holder/SpinnerViewHolder.kt
+++ app/src/main/java/com/mrkurilin/sample_project/adapter/holder/SpinnerViewHolder.kt
@@ -1,6 +1,97 @@
 package com.mrkurilin.sample_project.adapter.holder
 
 import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.Spinner
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.drawable.DrawableCompat
 import androidx.recyclerview.widget.RecyclerView
+import com.mrkurilin.sample_project.R
 
-class SpinnerViewHolder(view: View) : RecyclerView.ViewHolder(view)
+class SpinnerViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+    private val spinnerColor: Spinner = view.findViewById(R.id.spinner_color_spinner_widget)
+    private val spinnerShape: Spinner = view.findViewById(R.id.spinner_shape_spinner_widget)
+    private val spinnerContent: Spinner = view.findViewById(R.id.spinner_content_spinner_widget)
+    private val imageView: ImageView = view.findViewById(R.id.imageview_spinner_widget)
+    private val onItemSelectedListener by lazy { getOnSpinnerItemSelectedListener() }
+
+    private val colorMap = mapOf(
+        "Red" to R.color.red,
+        "Green" to R.color.green,
+        "Blue" to R.color.blue,
+    )
+
+    private val shapeMap = mapOf(
+        "Rectangle" to R.drawable.rectangle,
+        "Circle" to R.drawable.circle,
+    )
+
+    private val contentMap = mapOf(
+        "Android" to R.drawable.ic_baseline_android_24,
+        "Land scape" to R.drawable.ic_baseline_landscape_24,
+        "Self improvement" to R.drawable.ic_baseline_self_improvement_24,
+    )
+
+    init {
+        setAdapter(spinnerColor, colorMap)
+        setAdapter(spinnerShape, shapeMap)
+        setAdapter(spinnerContent, contentMap)
+
+        spinnerShape.onItemSelectedListener = onItemSelectedListener
+        spinnerColor.onItemSelectedListener = onItemSelectedListener
+        spinnerContent.onItemSelectedListener = onItemSelectedListener
+    }
+
+    private fun getOnSpinnerItemSelectedListener(): AdapterView.OnItemSelectedListener {
+        return object : AdapterView.OnItemSelectedListener {
+
+            override fun onItemSelected(
+                parent: AdapterView<*>,
+                view: View,
+                position: Int,
+                id: Long
+            ) {
+                when (parent) {
+                    spinnerColor -> setImageViewBackgroundColor()
+                    spinnerShape -> {
+                        imageView.background = ContextCompat.getDrawable(
+                            view.context,
+                            shapeMap[parent.selectedItem.toString()] ?: R.drawable.rectangle
+                        )
+                        setImageViewBackgroundColor()
+                    }
+                    spinnerContent -> imageView.setImageDrawable(
+                        ContextCompat.getDrawable(
+                            view.context,
+                            contentMap[parent.selectedItem.toString()]
+                                ?: R.drawable.ic_baseline_android_24
+                        )
+                    )
+                }
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {}
+        }
+    }
+
+    private fun setImageViewBackgroundColor() {
+        DrawableCompat.setTint(
+            imageView.background,
+            ContextCompat.getColor(
+                spinnerColor.context,
+                colorMap[spinnerColor.selectedItem.toString()] ?: R.color.red
+            )
+        )
+    }
+
+    private fun setAdapter(spinner: Spinner, map: Map<String, *>) {
+        spinner.adapter = ArrayAdapter(
+            imageView.context,
+            android.R.layout.simple_spinner_dropdown_item,
+            map.keys.toTypedArray()
+        )
+    }
+}
